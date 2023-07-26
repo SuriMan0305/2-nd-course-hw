4bcec57e97efbfe571de35b245ce7cb867e2b135
SuriMan0305 <mihail.levchenko.0305@mail.ru>
2023-07-26 05:30:52 +0300

    Доделал игру на JS

--- main.js
+++ main.js
@@ -26,9 +26,37 @@ const timeYear = (
   return result;
 };
 
-const rememberWords = (words = ["Яблоко","Груша","Дыня","Виноград","Персик","Апельсин","Мандарин"]) => {
-  alert(`Запомните порядок слов, будьте внимательны: ${words.join(", ")}`);
-  for (word of words) {
-    console.log(word.toUpperCase());
+const rememberWords = (words = ["Яблоко","Груша","Дыня","Виноград","Персик","Апельсин","Мандарин"].sort(() => Math.random() - 0.5)) => {
+  alert(`Запомните порядок слов в массиве, будьте внимательны: ${words.join(", ")}`);
+  // Сделал так чтобы первый элемент массива находился в первой части вопроса (от 1 до 4) а второй во второй (от 5 до 7) если считать не по индексу элемента.
+  let wordFirstIndex = Math.round(Math.random() * (words.length - 4));
+  let wordSecondIndex = Math.round(Math.random() * (words.length - 4) + 3.49);
+  //
+  firstAnswer = words[wordFirstIndex];
+  secondAnswer = words[wordSecondIndex];
+  // Использовать для проверки!!!
+  console.log(words);
+  console.log(firstAnswer);
+  console.log(secondAnswer);
+  // Использовать для проверки!!!
+  let answer = prompt(`Чему равнялся ${wordFirstIndex + 1}-й элемент массива?`);
+  let check = 0;
+  if (answer.toUpperCase() == firstAnswer.toUpperCase()) {
+    check++;
+  }
+  answer = prompt(`Чему равнялся ${wordSecondIndex + 1}-й элемент массива?`);
+  if (answer.toUpperCase() == secondAnswer.toUpperCase()) {
+    check++;
+  }
+  switch(check) {
+    case 0:
+      alert('К сожалению, вы не угадали ни одного слова');
+      break;
+    case 1:
+      alert('Вы были близки к победе!');
+      break;
+    case 2:
+      alert('Поздравляю! Вы отгадали оба слова!');
+      break;
   }
 };
